9d88d4b238d8ce4eb5918ae3ca5bb616450058e6
Hellen <hsantos@slideworks.cc>
2024-04-21 11:30:32 -0300

    feat: add error handler to scraper

--- src/scraper.ts
+++ src/scraper.ts
@@ -3,43 +3,51 @@ import { Product } from '@/entities/product';
 
 const scrapeTopSellers = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
     args: ['--start-maximized'],
   });
 
-  const page = await browser.newPage();
-  await page.goto('https://www.amazon.com.br/bestsellers', {
-    waitUntil: 'networkidle0',
-  });
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com.br/bestsellers', {
+      waitUntil: 'networkidle0',
+    });
 
-  const topSellers = await page.evaluate(() => {
-    const products: Product[] = [];
+    const topSellers = await page.evaluate(() => {
+      const products: Product[] = [];
 
-    const infos = Array.from(
-      document.querySelectorAll('.zg-carousel-general-faceout'),
-    ).slice(0, 3);
+      const infos = Array.from(
+        document.querySelectorAll('.zg-carousel-general-faceout'),
+      ).slice(0, 3);
 
-    for (const info of infos) {
-      const price = info?.querySelector('.a-color-price span')?.textContent;
-      const title = info?.querySelector('.a-link-normal span div')?.textContent;
-      const url = info?.querySelector('a')?.href;
-      const rate = info?.querySelector('.a-icon-alt')?.textContent;
+      for (const info of infos) {
+        const price = info?.querySelector('.a-color-price span')?.textContent;
+        const title = info?.querySelector(
+          '.a-link-normal span div',
+        )?.textContent;
+        const url = info?.querySelector('a')?.href;
+        const rate = info?.querySelector('.a-icon-alt')?.textContent;
 
-      const product: Product = {
-        title: title || 'indisponível',
-        price: price || 'indisponível',
-        rate: rate || 'indisponível',
-        url: url || 'indisponível',
-      };
-      products.push(product);
-    }
+        const product: Product = {
+          title: title || 'indisponível',
+          price: price || 'indisponível',
+          rate: rate || 'indisponível',
+          url: url || 'indisponível',
+        };
+        products.push(product);
+      }
 
-    return products;
-  });
+      return products;
+    });
 
-  await browser.close();
-  return topSellers;
+    return topSellers;
+  } catch (error) {
+    console.error('Scraping error:', error);
+    return [];
+  } finally {
+    await browser.close();
+  }
 };
 
 export default scrapeTopSellers;
